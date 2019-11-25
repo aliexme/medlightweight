81ea5e85536e713e0ee6b533ac2f3e5516838f40
Alexander Smirnov <al.smirnov996@gmail.com>
2019-11-25 09:57:45 +0300

    web-client: add typescript

--- web-client/webpack.config.js
+++ web-client/webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const PATH_SRC = path.resolve(__dirname, 'src')
@@ -33,6 +34,9 @@ module.exports = (env, options) => {
     },
     plugins: [
       new CleanWebpackPlugin(),
+      new ForkTsCheckerWebpackPlugin({
+        eslint: true,
+      }),
       new HtmlWebpackPlugin({
         template: path.resolve(PATH_SRC, 'index.html'),
         filename: 'index.html',
@@ -46,6 +50,15 @@ module.exports = (env, options) => {
     },
     module: {
       rules: [
+        {
+          test: /\.tsx?$/,
+          use: [{
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          }],
+        },
         {
           test: /\.(sa|sc|c)ss$/,
           use: [
